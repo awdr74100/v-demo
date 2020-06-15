1d19d6c878566168d4a2e184ef23369f1e6c4162
藍奕濡 <a78945612385238@gmail.com>
2020-06-16 01:01:43 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
+    <router-view name="aa"></router-view>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <button ></button>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
